7c6a766828a69b13a5e014b416defebcaa166c64
Ruslan Temirkhanov <rusl0424@gmail.com>
2019-08-01 03:55:31 -0400

    Refactored OAuth2 sign-in using spring security clientID and jose. Removed autoconfigure dependency.

--- src/main/java/com/goruslan/oauth/security/AppSecurity.java
+++ src/main/java/com/goruslan/oauth/security/AppSecurity.java
@@ -1,26 +1,26 @@
-package com.goruslan.oauth.security;
-
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-@Configuration
-@EnableWebSecurity
-@EnableOAuth2Sso
-public class AppSecurity extends WebSecurityConfigurerAdapter {
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-
-        http
-                .csrf().disable()
-                .authorizeRequests()
-                    .antMatchers("/").permitAll()
-                    .antMatchers("/login").permitAll()
-                .anyRequest().authenticated();
-
-    }
-
-}
+//package com.goruslan.oauth.security;
+//
+////import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//
+//@Configuration
+//@EnableWebSecurity
+////@EnableOAuth2Sso
+//public class AppSecurity extends WebSecurityConfigurerAdapter {
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//
+//        http
+//               .csrf().disable()
+//                .authorizeRequests()
+//                    .antMatchers("/").permitAll()
+//                    .antMatchers("/login").permitAll()
+//                .anyRequest().authenticated();
+//
+//    }
+//
+//}
