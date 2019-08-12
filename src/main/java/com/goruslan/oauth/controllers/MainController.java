f3b4a7163e26639b15db33a3ae69a00637be8677
Ruslan Temirkhanov <rusl0424@gmail.com>
2019-08-12 15:35:15 -0400

    Added RestTemplate to get Uuser's GitHub repositories

--- src/main/java/com/goruslan/oauth/controllers/MainController.java
+++ src/main/java/com/goruslan/oauth/controllers/MainController.java
@@ -10,8 +10,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.List;
+import java.util.LinkedHashMap;
 import java.security.Principal;
+import java.util.LinkedHashMap;
 
 @Controller
 public class MainController {
@@ -23,11 +27,23 @@ public class MainController {
     }
 
     @GetMapping("/private")
-    public String home(Model model) {
+    public String secret(Model model) {
         OAuth2User user = getCurrentUser();
         User newUser = new User(user.getAttributes().get("login").toString());
         model.addAttribute("user", newUser);
         return "private";
+    }
+
+    @GetMapping("/repo")
+    public String repos(Model model) {
+        OAuth2User user = getCurrentUser();
+        RestTemplate restTemplate = new RestTemplate();
+        List<LinkedHashMap<String, String>> reps = restTemplate.getForObject(user.getAttributes().get("repos_url").toString(), List.class);
+        for(LinkedHashMap<String,String>  repo : reps)
+        {
+            System.out.println(repo.get("name"));
+        }
+        return "repos";
     }
 
     @GetMapping("/")
